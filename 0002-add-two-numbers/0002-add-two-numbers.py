618c6d3455a60354866dc122d6d55a0c5266f5f5
ahmedbhy1 <156582334+ahmedbhy1@users.noreply.github.com>
2024-10-02 16:53:20 +0100

    Time: 51 ms (75.87%), Space: 16.7 MB (32.59%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -11,20 +11,17 @@ class Solution:
         rest = (l1.val + l2.val)//10
 
         while True:
-            if l1 != None:
-                l1=l1.next
-            if l2 != None:
-                l2=l2.next
 
-            v1=0
+            l1=l1.next if l1 else l1
+            l2=l2.next if l2 else l2
+            
             v2=0
-            if l1 == None and l2 == None :
+            if not l1 and not l2 :
                 break
-            if l1 == None:
-                v2 = l2.val
-            if l2 == None:
-                v1 = l1.val
-            if l1 != None and l2 != None:
+            v2 = 0 if l1 else l2.val
+            v1 = 0 if l2 else l1.val     
+
+            if l1 and l2:
                 v1 = l1.val
                 v2 = l2.val
 
