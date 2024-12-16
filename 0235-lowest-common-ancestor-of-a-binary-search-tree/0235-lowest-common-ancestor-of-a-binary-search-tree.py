07e389cf4efd4a726b5d8b6aa909bcb2a628ff33
ahmedbhy1 <156582334+ahmedbhy1@users.noreply.github.com>
2024-12-16 11:25:11 +0100

    Time: 76 ms (5.12%), Space: 21.6 MB (13.2%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,62 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        d = dict()
+        np = dict()
+    
+        def dfs(node: TreeNode,i : int):
+            if node == None:
+                return
+            np[node.val] = i 
+            test = False
+            if (node.left):
+                d[node.left.val] = node.val
+                test = True
+                dfs(node.left,i+1)
+            if (node.right != None):
+                d[node.right.val] = node.val
+                test = True
+                dfs(node.right,i+1)
+            if (test == False):
+                return
+
+        dfs(root,0)
+        
+        i = np[p.val]
+        j = np[q.val]
+
+        a = p.val
+        b = q.val
+        
+        print("did we enter to the while ?",i,j)
+        while i>=0 and j>=0:
+            print(i,j)
+            if (i==j):
+                if (a == b): 
+                    tr = TreeNode(a)
+                    return tr
+                i-=1
+                j-=1
+                a = d[a]
+                b = d[b] 
+                continue
+            if (i<j):
+                j-=1
+                print(b)
+                print(d)
+                b = d[b]
+                continue
+            if (i>j):
+                i-=1
+                a = d[a]
+                continue
+
+            
+        print(d)
+        print(np)
