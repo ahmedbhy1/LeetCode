df3c961f6e26dcecdfda84f06c430f0b22e7b5b3
ahmedbhy1 <156582334+ahmedbhy1@users.noreply.github.com>
2025-01-07 20:46:19 +0100

    Time: 0 ms (100%), Space: 18 MB (11.73%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,21 +1,19 @@
+
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if(s[0]=='0'): return 0
-        if len(s) == 1: return 1
-        if (int(s[0]) > 2 and int(s[1]) == 0):
-            return 0
+        if s[0] == '0': return 0
         n = len(s)
-        dp = [0] * (n)
-        dp [0] = 1
-        if (int(s[0]+s[1]) < 27 and int(s[1]) != 0):dp [1] = 2 
-        else:dp[1] = 1
-        for i in range(2,n):
-            if (int(s[i-1]+s[i]) < 27 and int(s[i])>0 and int(s[i-1])>0):dp [i] = dp[i-2] + dp[i-1]
-            else: 
-                if int(s[i]) == 0:
-                    if int(s[i-1])==0 or int(s[i-1])>2:
-                        return 0
-                    dp[i] = dp[i-2]
-                else:
-                    dp[i] = dp[i-1]
+        if n == 1: return 1
+        dp = [1, 1 if s[1] != '0' else 0]
+        if 10 <= int(s[:2]) <= 26: dp[1] += 1
+        
+        for i in range(2, n):
+            if s[i] == '0':
+                if s[i-1] in '12': dp.append(dp[-2])
+                else: return 0
+            elif 10 <= int(s[i-1:i+1]) <= 26:
+                dp.append(dp[-1] + dp[-2])
+            else:
+                dp.append(dp[-1])
+        
         return dp[-1]
